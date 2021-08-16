4e00ed11a4e1ddc85f4f6b570c3dae820cbdb4be
sangyunLEE <winnery93@gmail.com>
2021-08-16 23:00:23 +0900

    init push

---
+++ artoo/src/main/java/org/dagdak/artoo/ArtooApplication.java
@@ -0,0 +1,13 @@
+package org.dagdak.artoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtooApplication.class, args);
+    }
+
+}
---
+++ artoo/src/test/java/org/dagdak/artoo/ArtooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dagdak.artoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
